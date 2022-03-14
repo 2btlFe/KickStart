71d8d601144233aba6e61e467610b5bd4d1e0398
2btlFe <61321286+2btlFe@users.noreply.github.com>
2022-03-14 20:23:05 +0900

    Create Basic Coding.cpp

---
+++ Basic Coding.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define len(x) (int) (x).size()
+#define all(x) x.begin(), (x).end()
+#define endl "\n"
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+#define fi first
+#define se second
+
+int main()
+{
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  
+  int T;
+  cin >> T;
+
+  for(int t = 1; t <= T; t++){
+    auto ret;
+    
+    cout << "Case #" << t << ": " << ret << endl;
+  }
+}
